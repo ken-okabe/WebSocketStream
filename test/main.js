a997497ffa57c7b5120c958c0b5e4087a27cd7cb
David Braun <BallBearing@example.com>
2013-06-19 14:50:10 -0300

    Rewrite test as AMD module.

--- test/main.js
+++ test/main.js
@@ -3,141 +3,141 @@
 
 'use strict';
 
-var assert = require('assert2'),
-    events = require('events'),
-    stream = require('stream'),
-    util = require('util'),
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
-    WebSocketStream = require('./../lib/main'),
-    clientSocket,
-    clientStream,
-    string = "Come back to me.",
-    buffer = new Buffer([1, 2, 3]),
-    flushed,
-    messages = [],
+define(['assert2', 'events', 'stream', 'util', '../lib/main'],
+    function(assert, events, stream, util, WebSocketStream) {
 
-    // Shim until ArrayBufferView is exposed properly.  See:
-    // https://www.khronos.org/webgl/public-mailing-list/archives/1212/msg00121.html
-    // https://code.google.com/p/chromium/issues/detail?id=60449#c4
-    ArrayBufferView = ArrayBufferView
-        || (new Uint8Array(0)).__proto__.__proto__.constructor,
+    var clientSocket,
+        clientStream,
+        string = "Come back to me.",
+        buffer = new Buffer([1, 2, 3]),
+        flushed,
+        messages = [],
 
-    MockSocket = (function () {
-        function MockSocket () {
-            events.EventEmitter.call(this);
-            this.readyState = this.CLOSED;
-            this.bufferedAmount = 0;
-        }
+        // Shim until ArrayBufferView is exposed properly.  See:
+        // https://www.khronos.org/webgl/public-mailing-list/archives/1212/msg00121.html
+        // https://code.google.com/p/chromium/issues/detail?id=60449#c4
+        ArrayBufferView = ArrayBufferView
+            || (new Uint8Array(0)).__proto__.__proto__.constructor,
 
-        util.inherits(MockSocket, events.EventEmitter);
+        MockSocket = (function () {
+            function MockSocket () {
+                events.EventEmitter.call(this);
+                this.readyState = this.CLOSED;
+                this.bufferedAmount = 0;
+            }
 
-        MockSocket.prototype.OPEN = 1;
-        MockSocket.prototype.CLOSED = 3;
-        MockSocket.prototype.addEventListener = MockSocket.prototype.on;
+            util.inherits(MockSocket, events.EventEmitter);
 
-        MockSocket.prototype.close = function () {
-            this.readyState = this.CLOSED;
-            this.emit('close');
-        };
+            MockSocket.prototype.OPEN = 1;
+            MockSocket.prototype.CLOSED = 3;
+            MockSocket.prototype.addEventListener = MockSocket.prototype.on;
 
-        MockSocket.prototype.send = function (data) {
-            this.emit('message', {
-                type: data instanceof ArrayBufferView ? 'Binary' : 'Text',
-                data: data
-            });
-        };
+            MockSocket.prototype.close = function () {
+                this.readyState = this.CLOSED;
+                this.emit('close');
+            };
 
-        return MockSocket;
-    })();
+            MockSocket.prototype.send = function (data) {
+                this.emit('message', {
+                    type: data instanceof ArrayBufferView ? 'Binary' : 'Text',
+                    data: data
+                });
+            };
 
-function test2() {
-    var clientStream = new WebSocketStream(clientSocket);
+            return MockSocket;
+        })();
 
-    assert.doesNotEmit(clientStream, 'drain', function () {
+    function test2() {
+        var clientStream = new WebSocketStream(clientSocket);
+
+        assert.doesNotEmit(clientStream, 'drain', function () {
+            // Pretend that the write is flushed immediately.
+            clientSocket.bufferedAmount = 0;
+            clientStream.write(new Buffer(17 * 1024));
+        });
+    }
+
+    clientSocket = new MockSocket();
+    clientStream = new WebSocketStream(clientSocket, {objectMode: true});
+
+    // Save messages to an array as they come in.
+    clientStream.on('readable', function () {
+        messages.push(clientStream.read());
+    });
+
+    // Attempt a write before the socket is opened.  This should be buffered and
+    // sent later.
+    flushed = clientStream.write("pre-open");
+    assert(!flushed);
+
+    clientSocket.addEventListener('open', function () {
         // Pretend that the write is flushed immediately.
         clientSocket.bufferedAmount = 0;
-        clientStream.write(new Buffer(17 * 1024));
+        flushed = clientStream.write(string);
+        assert(flushed);
+
+        // Pretend that the write is buffered.
+        clientSocket.bufferedAmount = 42;
+        flushed = clientStream.write(buffer);
+        assert(!flushed);
+
+        // Pretend that the buffer is flushed in 200 ms.  We should see a
+        // 'drain' event.
+        setTimeout(function () {
+            clientSocket.bufferedAmount = 0;
+        }, 200);
+
+        clientStream.once('drain', function () {
+            assert.equal(clientSocket.bufferedAmount, 0);
+
+            // End the stream.
+            clientStream.end();
+        });
     });
-}
 
-clientSocket = new MockSocket();
-clientStream = new WebSocketStream(clientSocket, {objectMode: true});
+    clientSocket.readyState = clientSocket.OPEN;
 
-// Save messages to an array as they come in.
-clientStream.on('readable', function () {
-    messages.push(clientStream.read());
-});
+    // This should emit a drain event because 'write' returned false when we
+    // wrote before it was open.
+    assert.emits(clientStream, 'drain', function () {
+        clientSocket.emit('open');
+    });
 
-// Attempt a write before the socket is opened.  This should be buffered and
-// sent later.
-flushed = clientStream.write("pre-open");
-assert(!flushed);
+    clientStream.on('end', function () {
+        assert.deepEqual(messages, ["pre-open", string, buffer]);
 
-clientSocket.addEventListener('open', function () {
-    // Pretend that the write is flushed immediately.
-    clientSocket.bufferedAmount = 0;
-    flushed = clientStream.write(string);
-    assert(flushed);
+        assert.emits(clientStream, 'error', function () {
+            // Pretend there's an error.
+            clientSocket.emit('error');
+        });
 
-    // Pretend that the write is buffered.
-    clientSocket.bufferedAmount = 42;
-    flushed = clientStream.write(buffer);
-    assert(!flushed);
+        clientStream = new WebSocketStream(clientSocket);
 
-    // Pretend that the buffer is flushed in 200 ms.  We should see a 'drain'
-    // event.
-    setTimeout(function () {
+        // Closing the socket should close the stream.
+        clientSocket.close();
+    });
+
+    clientStream.on('close', function () {
+        // Pretend there is no bufferedAmount but the write should not be
+        // flushed when the socket is closed.
+
+        var drainEmitted = false;
+
+        clientStream.on('drain', function () {
+            drainEmitted = true;
+        });
+
         clientSocket.bufferedAmount = 0;
-    }, 200);
+        flushed = clientStream.write(string);
+        assert(!flushed);
 
-    clientStream.once('drain', function () {
-        assert.equal(clientSocket.bufferedAmount, 0);
+        // 'drain' should not be emitted when the socket is closed.
+        setTimeout(function () {
+            assert(!drainEmitted);
+        }, 200);
 
-        // End the stream.
-        clientStream.end();
+        test2();
     });
-});
-
-clientSocket.readyState = clientSocket.OPEN;
-
-// This should emit a drain event because 'write' returned false when we wrote
-// before it was open.
-assert.emits(clientStream, 'drain', function () {
-    clientSocket.emit('open');
-});
-
-clientStream.on('end', function () {
-    assert.deepEqual(messages, ["pre-open", string, buffer]);
-
-    assert.emits(clientStream, 'error', function () {
-        // Pretend there's an error.
-        clientSocket.emit('error');
-    });
-
-    clientStream = new WebSocketStream(clientSocket);
-
-    // Closing the socket should close the stream.
-    clientSocket.close();
-});
-
-clientStream.on('close', function () {
-    // Pretend there is no bufferedAmount but the write should not be
-    // flushed when the socket is closed.
-
-    var drainEmitted = false;
-
-    clientStream.on('drain', function () {
-        drainEmitted = true;
-    });
-
-    clientSocket.bufferedAmount = 0;
-    flushed = clientStream.write(string);
-    assert(!flushed);
-
-    // 'drain' should not be emitted when the socket is closed.
-    setTimeout(function () {
-        assert(!drainEmitted);
-    }, 200);
-
-    test2();
 });
